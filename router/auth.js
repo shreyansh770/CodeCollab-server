7c028fc0d3ad1e6fae7d005aaf43455fb12cbe41
shreyansh770 <shreyanshthakur1@gmail.com>
2024-01-29 22:16:50 +0530

    register and logout routes defined

--- router/auth.js
+++ router/auth.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { enterInterview, createUser, login } = require("../controller/auth.controller");
+const { enterInterview, createUser, login, register, logout } = require("../controller/auth.controller");
+const upload = require("../config/multer");
 const authRouter = express.Router();
 
 authRouter.route("/createUser").post(createUser)
+authRouter.route("/register").post(upload.single('file'),register,createUser)
 authRouter.route("/login").post(login)
+authRouter.route("/logout").post(logout)
 authRouter.post("/enterInterview", enterInterview);
 
 module.exports = authRouter;
