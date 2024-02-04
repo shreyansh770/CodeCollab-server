3ee5e11504cc818228739cd23b5e3d1358add522
shreyansh770 <shreyanshthakur1@gmail.com>
2024-02-04 18:59:48 +0530

    planModel added

---
+++ model/planModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const connectToMongoDB = require("../utils/mongo");
+
+connectToMongoDB();
+
+
+const planSchema = new mongoose.Schema({
+    planName: {
+      type: String,
+      required: true,
+      unique: true, // If you want each plan to have a unique name
+    },
+    planPrice: {
+      type: String,
+      required: true,
+    },
+    noOfInterviews: {
+      type: Number,
+      required: true,
+    },
+});
+
+
+const planModel = mongoose.model("planModel",planSchema)
+
+module.exports = planModel;
